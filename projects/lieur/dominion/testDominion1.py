93782de29af7b9a9b032d7e9f0de5aa3a86c121a
lieuraymond <lieu.raymond@gmail.com>
2020-01-19 20:44:50 -0800

    Test cases added

--- projects/lieur/dominion/testDominion1.py
+++ projects/lieur/dominion/testDominion1.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
+""""
 Created on Saturday, January 18, 2020
 
 @author: lieur
-"""
+
+This test case sets the random supply box to null, meaning only victory cards
+and money exist.  This allows us to test the coin buying and victory conditions of
+when the provinces are all sold out.  
+"""""
 
 import Dominion
 import testUtility
@@ -16,16 +20,19 @@ player_names = ["Annie", "*Ben", "*Carla"]
 # Set number of curses and victory cards
 nV, nC = testUtility.set_vc_number(player_names)
 
-# Define box
+# Define box and supply_order
 box, supply_order = testUtility.define_box(nV)
 
-# Set supply
+#Test no box
+box = {}
+
+# Choose and set supply cards
 supply = testUtility.set_supply(box, player_names, nV, nC)
 
-# initialize the trash
+# Initialize the trash
 trash = []
 
-# Costruct the Player objects
+# Construct the Player objects
 players = testUtility.set_players(player_names)
 
 # Play the game
--- projects/lieur/dominion/testDominion2.py
+++ projects/lieur/dominion/testDominion2.py
@@ -1,52 +1,60 @@
 # -*- coding: utf-8 -*-
-"""
+""""
 Created on Saturday, January 18, 2020
 
 @author: lieur
-"""
+
+This test case sets silver and gold to 0, which in most cases prevent the computer from
+buying provinces.  This tests to see if the game ends when one more supply car hits 0 (since
+silver and gold are already at 0 and the game ends when 3 supply deck hits 0)
+"""""
 
 import Dominion
 import testUtility
 import random
 from collections import defaultdict
 
-#Get player names
-player_names = ["Annie","*Ben","*Carla"]
+# Get player names
+player_names = ["Annie", "*Ben", "*Carla"]
 
-#Set number of curses and victory cards
+# Set number of curses and victory cards
 nV, nC = testUtility.set_vc_number(player_names)
 
-#Define box
+# Define box and supply_order
 box, supply_order = testUtility.define_box(nV)
 
-#Set supply
+
+# Choose and set supply cards
 supply = testUtility.set_supply(box, player_names, nV, nC)
 
-#initialize the trash
+# Initialize the trash
 trash = []
 
-#Costruct the Player objects
+supply["Copper"]=[Dominion.Copper()]*(60-len(player_names)*7)
+supply["Silver"]=[Dominion.Silver()]*0
+supply["Gold"]=[Dominion.Gold()]*0
+# Construct the Player objects
 players = testUtility.set_players(player_names)
 
-#Play the game
-turn  = 0
+# Play the game
+turn = 0
 while not Dominion.gameover(supply):
-    turn += 1    
-    print("\r")    
+    turn += 1
+    print("\r")
     for value in supply_order:
-        print (value)
+        print(value)
         for stack in supply_order[value]:
             if stack in supply:
-                print (stack, len(supply[stack]))
+
+                print(stack, len(supply[stack]))
     print("\r")
     for player in players:
-        print (player.name,player.calcpoints())
-    print ("\rStart of turn " + str(turn))    
+        print(player.name, player.calcpoints())
+    print("\rStart of turn " + str(turn))
     for player in players:
         if not Dominion.gameover(supply):
             print("\r")
-            player.turn(players,supply,trash)
-            
+            player.turn(players, supply, trash)
 
-#Final score
+# Final score
 testUtility.display_game_results(players)
