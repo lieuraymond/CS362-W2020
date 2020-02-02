2762ff90f53ab9ef8d51790efef8221485dac324
lieuraymond <lieu.raymond@gmail.com>
2020-02-01 21:06:12 -0800

    Update testUtility.py

--- projects/lieur/dominion/testUtility.py
+++ projects/lieur/dominion/testUtility.py
@@ -79,9 +79,11 @@ def set_players(player_names):
             players.append(Dominion.ComputerPlayer(name[1:]))
         elif name[0] == "^":
             players.append(Dominion.TablePlayer(name[1:]))
-        else:
+        else
             players.append(Dominion.Player(name))
     return players
+
+#define play game
 def play_game(supply, supply_order, players, trash):
     turn = 0
     while not Dominion.gameover(supply):
